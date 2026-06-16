2e012fdfbb4097c868203e036f2be0f27ea10ee2
DasShorty <anthony.timmel@dasshorty.de>
2026-06-16 13:15:59 +0200

    Add line separation between rows

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 typedef void (*ColumnConsumer)(int, int, char*);
 
 void printRow(int rowIndex, int totalColumns, ColumnConsumer colConsumer) {
-    printf("Row %d:[",rowIndex);
 
     char cellBuffer[50];
 
@@ -14,23 +13,28 @@ void printRow(int rowIndex, int totalColumns, ColumnConsumer colConsumer) {
         printf("%s", cellBuffer);
     }
 
-    printf("]\n");
+    printf("\n");
 }
 
-void coordinateFormatConsumer(int row, int col, char* outputBuffer) {
-    sprintf(outputBuffer, "(R%d,C%d)", row, col);
-}
-
-void matrixFormatConsumer(int row, int col, char* outputBuffer) {
-    sprintf(outputBuffer, "[%d-%d]", row, col);
-}
+// void coordinateFormatConsumer(int row, int col, char* outputBuffer) {
+//     sprintf(outputBuffer, "(R%d,C%d)", row, col);
+// }
+//
+// void matrixFormatConsumer(int row, int col, char* outputBuffer) {
+//     sprintf(outputBuffer, "[%d-%d]", row, col);
+// }
 
 void chessFieldConsumer(int row, int col, char* outputBuffer) {
     if (col == 0) {
-        sprintf(outputBuffer, "| |");
+        sprintf(outputBuffer, "|  |");
     } else {
-        sprintf(outputBuffer, " |");
+        sprintf(outputBuffer, "  |");
     }
+
+}
+
+void chessFieldLineConsumer(int row, int col, char* outputBuffer) {
+    sprintf(outputBuffer, "---");
 
 }
 
@@ -38,22 +42,24 @@ int main() {
 
     int totalColumns = 8;
 
-    printf("--- Using Coordinate Consumer ---\n");
-
-    for (int i = 0; i < totalColumns; i++) {
-        printRow(i, totalColumns, coordinateFormatConsumer);
-    }
-
-    printf("\n--- Using Matrix Consumer ---\n");
-
-    for (int i = 0; i < totalColumns; i++) {
-        printRow(i, totalColumns, matrixFormatConsumer);
-    }
+    // printf("--- Using Coordinate Consumer ---\n");
+    //
+    // for (int i = 0; i < totalColumns; i++) {
+    //     printRow(i, totalColumns, coordinateFormatConsumer);
+    // }
+    //
+    // printf("\n--- Using Matrix Consumer ---\n");
+    //
+    // for (int i = 0; i < totalColumns; i++) {
+    //     printRow(i, totalColumns, matrixFormatConsumer);
+    // }
 
     printf("\n--- Using Chess Consumer ---\n");
 
+    printRow(0, totalColumns, chessFieldLineConsumer);
     for (int i = 0; i < totalColumns; i++) {
         printRow(i, totalColumns, chessFieldConsumer);
+        printRow(-1, totalColumns, chessFieldLineConsumer);
     }
 
     return 0;
